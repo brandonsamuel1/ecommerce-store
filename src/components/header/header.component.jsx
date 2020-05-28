44ef4b1d374a74b0c2b1c0cd3ecaeb0ac0032edb
Brandon <brandonsamuel72@gmail.com>
2020-05-27 21:45:52 -0700

    refactored signout code to use sagas

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,12 +8,13 @@ import { auth } from '../../firebase/firebase.utils'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { selectCurrentUser } from '../../redux/user/user.selector'
+import { signOutStart } from '../../redux/user/user.actions'
 
 import { HeaderContainer, OptionsContainer, OptionDiv, OptionLink, LogoContainer } from './header.styles'
 
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
     <HeaderContainer>
         <LogoContainer to="/">
             <Logo className='logo' />
@@ -26,7 +27,7 @@ const Header = ({ currentUser, hidden }) => (
                 CONTACT
             </OptionLink>
             {currentUser ? (
-                <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+                <OptionDiv onClick={signOutStart}>SIGN OUT</OptionDiv>
             ) : (
                     <OptionLink to="/signin">SIGN IN</OptionLink>
                 )}
@@ -41,4 +42,8 @@ const mapStateToProps = createStructuredSelector({
     hidden: selectCartHidden
 })
 
-export default connect(mapStateToProps)(Header)
+const mapDispatchToProps = dispatch => ({
+    signOutStart: () => dispatch(signOutStart())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
